56ed6db38f890fad19ac4f788086811ed2d2e4d8
Тутункин Владислав <vladislav-login94@yandex.ru>
2023-09-10 22:14:52 +0300

    Edit models.py and README.md

--- backend/users/models.py
+++ backend/users/models.py
@@ -23,6 +23,7 @@ class MyUserManager(BaseUserManager):
         """Create and save a regular User with the given email and password."""
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
+        extra_fields.setdefault('is_active', False)
         return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password, **extra_fields):
