6f4e6933628ab3cdbf007bd10c1007359cfaf40f
sapphirehead <djey36@gmail.com>
2023-09-16 16:10:42 +0300

    fixed some docs-tests

--- backend/documents/models.py
+++ backend/documents/models.py
@@ -2,7 +2,6 @@ from django.core.validators import MinValueValidator
 from django.db import models
 from users.models import User
 
-
 CATEGORY_CHOICES = (
     ('diplomas', 'Дипломы'),
     ('certificates', 'Сертификаты'),
