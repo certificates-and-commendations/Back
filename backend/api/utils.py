8860a354c596169d3fbdb7074326effa2a82a3cc
deamanda <deamanda@ya.ru>
2023-09-18 16:49:07 +0400

    pdf generation v1

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -6,6 +6,10 @@ from django.core.files import File
 from django.core.files.base import ContentFile
 from documents.models import Font
 from PIL import Image, ImageDraw, ImageFont
+from reportlab.lib.utils import ImageReader
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfgen.canvas import Canvas
 from rest_framework import serializers
 
 
@@ -20,57 +24,81 @@ class Base64ImageField(serializers.ImageField):
         return super().to_internal_value(data)
 
 
+def image_draw(background, elements):
+    background_im = Image.open(background)
+    width, height = background_im.size
+    im = Image.new('RGBA', size=(width, height))
+    im.paste(background_im)
+    width //= 2
+    height //= 2
+    for element in elements:
+        with Image.open(element.image) as foreground:
+            foreground = foreground.convert('RGBA')
+            im.paste(foreground, (element.coordinate_x + int(width),
+                                  element.coordinate_y + int(height)),
+                     foreground)
+    return im
+
+
 def create_thumbnail(document):
     if document.is_horizontal:
         size = 304, 192
     else:
         size = 304, 460
-    with Image.open(document.background) as backgound:
-        im = backgound.copy()
-        width, height = im.size
-        width /= 2
-        height /= 2
-        draw = ImageDraw.Draw(im)
-        texts = document.textfield_set.all()
-        for text in texts:
-            ff = Font.objects.get(font=text.font, is_bold=text.is_bold,
-                                  is_italic=text.is_italic)
-            font = ImageFont.truetype(
-                ff.font_file,
-                text.font_size)
-            draw.text(
-                (text.coordinate_x + width, text.coordinate_y + height),
-                text.text,
-                font=font,
-                fill=text.font_color,
-                align=text.align)
-            if text.text_decoration == 'none':
-                continue
-            _, _, right, bottom = font.getbbox(text.text)
-            if text.text_decoration == 'underline':
-                line_y = (text.coordinate_y + height
-                          + (bottom - font.getmetrics()[1]) * 1.05)
-            if text.text_decoration == 'strikethrough':
-                line_y = (text.coordinate_y + height
-                          + (font.getmetrics()[0]
-                             - font.getmetrics()[1]))
-            draw.line([
-                (text.coordinate_x + width, line_y),
-                (text.coordinate_x + width + right, line_y)],
-                fill=text.font_color, width=2)
-        elements = document.element_set.all()
-        for element in elements:
-            with Image.open(element.image) as foreground:
-                im = im.convert('RGBA')
-                foreground = foreground.convert('RGBA')
-                im.paste(foreground,
-                         (element.coordinate_x + int(width), element.coordinate_y + int(height) ),
-                         foreground)
-        im = im.convert('RGB')
-        im_resized = im.resize(size)
-        thumb_io = BytesIO()
-        im_resized.save(thumb_io, 'JPEG', quality=95)
-        document.thumbnail = File(
-            thumb_io,
-            name=os.path.basename(document.background.name))
-        document.save()
+    im = image_draw(document.background, document.element_set.all())
+    width, height = im.size
+    width /= 2
+    height /= 2
+    draw = ImageDraw.Draw(im)
+    texts = document.textfield_set.all()
+    for text in texts:
+        ff = Font.objects.get(font=text.font, is_bold=text.is_bold,
+                              is_italic=text.is_italic)
+        font = ImageFont.truetype(ff.font_file, text.font_size)
+        draw.text((text.coordinate_x + width, text.coordinate_y + height),
+                  text.text, font=font, fill=text.font_color, align=text.align)
+        if text.text_decoration == 'none':
+            continue
+        _, _, right, bottom = font.getbbox(text.text)
+        if text.text_decoration == 'underline':
+            line_y = (text.coordinate_y + height
+                      + (bottom - font.getmetrics()[1]) * 1.05)
+        if text.text_decoration == 'strikethrough':
+            line_y = (text.coordinate_y + height + (font.getmetrics()[0]
+                                                    - font.getmetrics()[1]))
+        draw.line([
+            (text.coordinate_x + width, line_y),
+            (text.coordinate_x + width + right, line_y)],
+            fill=text.font_color, width=2)
+    im = im.convert('RGB')
+    im_resized = im.resize(size)
+    thumb_io = BytesIO()
+    im_resized.save(thumb_io, 'JPEG', quality=95)
+    document.thumbnail = File(thumb_io,
+                              name=os.path.basename(document.background.name))
+    document.save()
+
+
+def create_pdf(document):
+    background = image_draw(document.background, document.element_set.all())
+    doc_width, doc_height = background.size
+    canvas = Canvas(f'{document.title}.pdf', pagesize=background.size)
+    print(doc_width, doc_height)
+    width = doc_width / 2
+    height = doc_height / 2
+    canvas.drawImage(ImageReader(background), 0, 0)
+    texts = document.textfield_set.all()
+    for text in texts:
+        font = Font.objects.get(font=text.font, is_bold=text.is_bold,
+                                is_italic=text.is_italic)
+        rgb = (int(text.font_color[x - 2:x], 16) for x in range(3, 8, 2))
+        canvas.setFillColorRGB(*rgb)
+        pdfmetrics.registerFont(TTFont(font.font, font.font_file.path))
+        face = pdfmetrics.getFont(font.font).face
+        string_height = (face.ascent - face.descent) / 1000 * text.font_size
+        canvas.setFont(f'{font.font}', text.font_size)
+        canvas.drawString(text.coordinate_x + width,
+                          (doc_height - text.coordinate_y - height
+                           - string_height), text.text)
+    canvas.showPage()
+    canvas.save()
