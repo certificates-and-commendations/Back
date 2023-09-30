cd7b5c7bf5197bec41399681b2d992d6e25e82c1
Vandanova Mariya <handarkin@gmail.com>
2023-09-30 16:24:40 +0800

    is_horizontal filter upgrade

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -5,7 +5,10 @@ from documents.models import Category, Document
 
 class DocumentFilter(django_filters.FilterSet):
     """Фильтр документов по расположению/категории"""
-    is_horizontal = django_filters.BooleanFilter()
+    is_horizontal = django_filters.BooleanFilter(
+        field_name='is_horizontal',
+        widget=forms.CheckboxSelectMultiple
+    )
     category = django_filters.ModelMultipleChoiceFilter(
         field_name='category__slug',
         to_field_name='slug',
